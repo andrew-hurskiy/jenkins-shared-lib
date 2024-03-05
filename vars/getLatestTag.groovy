0fccb32f3382442245cd3bdc3ab9f11cd320d906
Andriy Hurskiy <andriy.hurskiy@inventify.com>
2024-03-05 22:43:08 +0200

    a2

---
+++ vars/getLatestTag.groovy
@@ -0,0 +1,25 @@
+#!/usr/bin/env groovy
+
+def call(
+    String app_name,
+    String repo_type,
+    Boolean is_debug_on = false
+    ) {
+        
+    String base_url = 'https://nexu.taf.nl:8443/service/rest/repository/browse/docker-private/v2/taf/'
+    String final_url = base_url + app_name + '/' + repo_type + '/tags'
+    
+
+    String latest_tag = sh(
+        script: "curl -s $final_url | grep -Eo '[[:digit:]]+\\.[[:digit:]]+\\.[[:digit:]]+' | sort -rVu | head -n 1",
+        returnStdout: true
+    ).trim()
+    
+    if (is_debug_on) {
+        echo "final_url -> ${final_url}"
+        echo "latest_tag -> ${latest_tag}"
+    }
+
+    return latest_tag
+    }
+
